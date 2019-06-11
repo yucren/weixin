680d5021b826b34298069636a7389f840c624751
John Doe <539928505@qq.com>
2019-06-11 15:43:33 +0800

    添加项目文件。

---
+++ Sample_2/CustomMessageHandler/CustomMessageHandler.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Web.Configuration;
+using Senparc.Weixin.MP.Context;
+using Senparc.Weixin.MP.Entities;
+using Senparc.Weixin.MP.MessageHandlers;
+using Senparc.Weixin.MP.Helpers;
+namespace Sample_2
+{
+    /// <summary>
+    /// 自定义MessageHandler
+    /// 把MessageHandler作为基类，重写对应请求的处理方法
+    /// </summary>
+    public partial class CustomMessageHandler : MessageHandler<MessageContext>
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="inputStream"></param>
+        /// <param name="maxRecordCount"></param>
+        public CustomMessageHandler(Stream inputStream, int maxRecordCount = 0)
+            : base(inputStream, maxRecordCount)
+        {
+            //这里设置仅用于测试，实际开发可以在外部更全局的地方设置，
+            //比如MessageHandler<MessageContext>.GlobalWeixinContext.ExpireMinutes = 3。
+            WeixinContext.ExpireMinutes = 3;
+        }
+        public override void OnExecuting()
+        {
+            //测试MessageContext.StorageData
+            if (CurrentMessageContext.StorageData == null)
+            {
+                CurrentMessageContext.StorageData = 0;
+            }
+            base.OnExecuting();
+        }
+        public override void OnExecuted()
+        {
+            base.OnExecuted();
+            CurrentMessageContext.StorageData = ((int)CurrentMessageContext.StorageData) + 1;
+        }
+        /// <summary>
+        /// 处理文字请求
+        /// </summary>
+        /// <returns></returns>
+        protected override IResponseMessageBase OnTextRequest(RequestMessageText requestMessage)
+        {
+            //注意：下面泛型ResponseMessageText即返回给客户端的类型，可以根据自己的需要填写ResponseMessageNews等不同类型。
+            var responseMessage = CreateResponseMessage<ResponseMessageText>();
+
+            var result = new StringBuilder();
+            result.AppendFormat("您刚才发送了文字信息：{0}\r\n\r\n", requestMessage.Content);
+
+            if (CurrentMessageContext.RequestMessages.Count > 1)
+            {
+                result.AppendFormat("您刚才还发送了如下消息（{0}/{1}）：\r\n", CurrentMessageContext.RequestMessages.Count, CurrentMessageContext.StorageData);
+                for (int i = CurrentMessageContext.RequestMessages.Count - 2; i >= 0; i--)
+                {
+                    var historyMessage = CurrentMessageContext.RequestMessages[i];
+                    result.AppendFormat("{0} 【{1}】{2}\r\n",
+                                        historyMessage.CreateTime.ToShortTimeString(),
+                                        historyMessage.MsgType.ToString(),
+                                        (historyMessage is RequestMessageText)
+                                            ? (historyMessage as RequestMessageText).Content
+                                            : "[非文字类型]"
+                        );
+                }
+                result.AppendLine("\r\n");
+            }
+            result.AppendFormat("如果您在{0}分钟内连续发送消息，记录将被自动保留（当前设置：最多记录{1}条）。过期后记录将会自动清除。\r\n", WeixinContext.ExpireMinutes, WeixinContext.MaxRecordCount);
+            result.AppendLine("\r\n");
+            result.AppendLine("您还可以发送【位置】【图片】【语音】【视频】等类型的信息（注意是这几种类型，不是这几个文字），查看不同格式的回复。");
+            responseMessage.Content = result.ToString();
+            return responseMessage;
+        }
+        /// <summary>
+        /// 处理位置请求
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
+        protected override IResponseMessageBase OnLocationRequest(RequestMessageLocation requestMessage)
+        {
+            var responseMessage = CreateResponseMessage<ResponseMessageText>();
+            responseMessage.Content = string.Format("您刚才发送了地理位置信息。Location_X：{0}，Location_Y：{1}，Scale：{2}，标签：{3}",
+                              requestMessage.Location_X, requestMessage.Location_Y,
+                              requestMessage.Scale, requestMessage.Label);
+            return responseMessage;
+        }
+        /// <summary>
+        /// 处理图片请求
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
+        protected override IResponseMessageBase OnImageRequest(RequestMessageImage requestMessage)
+        {
+            var responseMessage = CreateResponseMessage<ResponseMessageNews>();
+            responseMessage.Articles.Add(new Article()
+            {
+                Title = "您刚才发送了图片信息",
+                Description = "您发送的图片将会显示在边上",
+                PicUrl = requestMessage.PicUrl,
+                Url = "http://www.qxuninfo.com"
+            });
+            responseMessage.Articles.Add(new Article()
+            {
+                Title = "第二条",
+                Description = "第二条带连接的内容",
+                PicUrl = requestMessage.PicUrl,
+                Url = "http://www.qxuninfo.com"
+            });
+            return responseMessage;
+        }
+        /// <summary>
+        /// 处理语音请求
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
+        protected override IResponseMessageBase OnVoiceRequest(RequestMessageVoice requestMessage)
+        {
+            var responseMessage = CreateResponseMessage<ResponseMessageMusic>();
+            responseMessage.Music.MusicUrl = "http://www.qxuninfo.com/music.mp3";
+            responseMessage.Music.Title = "这里是一条音乐消息";
+            responseMessage.Music.Description = "时间都去哪儿了";
+            return responseMessage;
+        }
+        /// <summary>
+        /// 处理视频请求
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
+        protected override IResponseMessageBase OnVideoRequest(RequestMessageVideo requestMessage)
+        {
+            var responseMessage = CreateResponseMessage<ResponseMessageText>();
+            responseMessage.Content = "您发送了一条视频信息，ID：" + requestMessage.MediaId;
+            return responseMessage;
+        }
+        /// <summary>
+        /// 处理链接消息请求
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
+        protected override IResponseMessageBase OnLinkRequest(RequestMessageLink requestMessage)
+        {
+            var responseMessage = CreateResponseMessage<ResponseMessageText>();
+            responseMessage.Content = string.Format(@"您发送了一条连接信息：
+Title：{0}
+Description:{1}
+Url:{2}", requestMessage.Title, requestMessage.Description, requestMessage.Url);
+            return responseMessage;
+        }
+        /// <summary>
+        /// 处理事件请求（这个方法一般不用重写，这里仅作为示例出现。除非需要在判断具体Event类型以外对Event信息进行统一操作
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
+        protected override IResponseMessageBase OnEventRequest(RequestMessageEventBase requestMessage)
+        {
+            var eventResponseMessage = base.OnEventRequest(requestMessage);//对于Event下属分类的重写方法，见：CustomerMessageHandler_Events.cs
+            return eventResponseMessage;
+        }
+        protected override IResponseMessageBase DefaultResponseMessage(IRequestMessageBase requestMessage)
+        {     
+            //所有没有被处理的消息会默认返回这里的结果
+            var responseMessage = this.CreateResponseMessage<ResponseMessageText>();
+            responseMessage.Content = "这条消息来自DefaultResponseMessage。";
+            return responseMessage;
+        }
+    }
+}
---
+++ Sample_2/CustomMessageHandler/CustomMessageHandler_Events.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using Senparc.Weixin.MP.Context;
+using Senparc.Weixin.MP.Entities;
+using Senparc.Weixin.MP.Helpers;
+using Senparc.Weixin.MP.MessageHandlers;
+namespace Sample_2
+{
+    /// <summary>
+    /// 自定义MessageHandler
+    /// </summary>
+    public partial class CustomMessageHandler : MessageHandler<MessageContext>
+    {
+        /// <summary>
+        /// 微信客户端（通过微信服务器）自动发送过来的位置信息事件
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
+        protected override IResponseMessageBase OnEvent_LocationRequest(RequestMessageEvent_Location requestMessage)
+        {
+            var responseMessage = CreateResponseMessage<ResponseMessageText>();
+            responseMessage.Content = "这里写什么都无所谓，比如：上帝爱你！";
+            return responseMessage;//这里也可以返回null（需要注意写日志时候null的问题）
+        }
+        /// <summary>
+        /// 订阅（关注）事件
+        /// </summary>
+        /// <returns></returns>
+        protected override IResponseMessageBase OnEvent_SubscribeRequest(RequestMessageEvent_Subscribe requestMessage)
+        {
+            var responseMessage = CreateResponseMessage<ResponseMessageText>();
+            responseMessage.Content = @"您可以发送【文字】【位置】【图片】【语音】等不同类型的信息，查看不同格式的回复。
+                     您也可以直接点击菜单查看各种类型的回复。";
+            return responseMessage;
+        }
+        /// <summary>
+        /// 退订
+        /// 实际上用户无法收到非订阅账号的消息，所以这里可以随便写。
+        /// unsubscribe事件的意义在于及时删除网站应用中已经记录的OpenID绑定，消除冗余数据。并且关注用户流失的情况。
+        /// </summary>
+        /// <returns></returns>
+        protected override IResponseMessageBase OnEvent_UnsubscribeRequest(RequestMessageEvent_Unsubscribe requestMessage)
+        {
+            var responseMessage = base.CreateResponseMessage<ResponseMessageText>();
+            responseMessage.Content = "有空再来";
+            return responseMessage;
+        }
+        /// <summary>
+        /// 自定义菜单点击事件
+        /// </summary>
+        /// <returns></returns>
+        protected override IResponseMessageBase OnEvent_ClickRequest(RequestMessageEvent_Click requestMessage)
+        {
+            IResponseMessageBase reponseMessage = null;
+            //菜单点击，需要跟创建菜单时的Key匹配
+            switch (requestMessage.EventKey)
+            {
+                case "OneClick":
+                    {
+                        var strongResponseMessage = CreateResponseMessage<ResponseMessageText>();
+                        strongResponseMessage.Content = "您点击了底部按钮。";
+                        reponseMessage = strongResponseMessage;
+                    }
+                    break;
+                case "SubClickRoot_Text":
+                    {
+                        var strongResponseMessage = CreateResponseMessage<ResponseMessageText>();
+                        strongResponseMessage.Content = "您点击了子菜单按钮。";
+                        reponseMessage = strongResponseMessage;
+                    }
+                    break;
+            }
+            return reponseMessage;
+        }
+        /// <summary>
+        /// 扫描带参数二维码事件
+        /// 实际上用户无法收到非订阅账号的消息，所以这里可以随便写。
+        /// scan事件的意义在于获取扫描二维码中包含的参数，便于识别和统计用户扫描了哪个二维码。
+        /// </summary>
+        /// <returns></returns>
+        protected override IResponseMessageBase OnEvent_ScanRequest(RequestMessageEvent_Scan requestMessage)
+        {
+            var responseMessage = base.CreateResponseMessage<ResponseMessageText>();
+            responseMessage.Content = "感谢扫码";
+            return responseMessage;
+        }
+    }
+}
